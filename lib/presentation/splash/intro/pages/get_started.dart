3d0aeb44cfcb506ef618708951059dcbc59a5553
Leonardo Gonçalves <leonardo.elcaminante@gmail.com>
2024-12-08 22:38:39 +0100

    done land page

--- lib/presentation/splash/intro/pages/get_started.dart
+++ lib/presentation/splash/intro/pages/get_started.dart
@@ -30,48 +30,54 @@ class GetStartedPage extends StatelessWidget {
           Container(
               color: Colors.black.withOpacity(0.15) //operates like a filter
               ),
-          Column(
-            children: [
-              const SizedBox(
-                width: 10.0, // Para espaço horizontal
-                height: 10.0, // Para espaço vertical
-              ),
-              Align(
-                  alignment: Alignment.topCenter,
-                  child: SvgPicture.asset(AppVectors.spotifyLogo)),
-              const Spacer(),
-              const Text(
-                'Enjoy Listening to Music',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 18),
-              ),
-              const SizedBox(
-                height: 21,
-              ),
-              const Text(
-                'Enjoy the best music from ubi!\n our special music here is "So fe Irmao"',
-                style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: AppColors.grey,
-                    fontSize: 13),
-                textAlign: TextAlign.center,
-              ),
-              //button shld be bellow the 2 text promts
-              const SizedBox(
-                height: 20,
-              ),
-              BasicAppButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                const ChooseModePage()));
-                  }, //Create the ChoseModePage
-                  title: 'Get Started'),
-            ],
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
+            child: Column(
+              children: [
+                const SizedBox(
+                  width: 10.0, // Para espaço horizontal
+                  height: 10.0, // Para espaço vertical
+                ),
+                Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(AppVectors.spotifyLogo)),
+                const Spacer(),
+                const Text(
+                  'Enjoy Listening to Music',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18),
+                ),
+                const SizedBox(
+                  height: 21,
+                ),
+                const Text(
+                  'Enjoy the best music from ubi!\n our special music here is "So fe Irmao"',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.grey,
+                      fontSize: 13),
+                  textAlign: TextAlign.center,
+                ),
+                //button shld be bellow the 2 text promts
+                const SizedBox(
+                  height: 20,
+                ),
+                BasicAppButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  const ChooseModePage()));
+                    }, //Create the ChoseModePage
+                    title: 'Get Started'),
+              ],
+            ),
           ),
         ],
       ),
