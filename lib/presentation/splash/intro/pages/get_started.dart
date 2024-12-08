0a1974ca60269508cbc6c8aad5282fa9191f1181
Leonardo Gonçalves <leonardo.elcaminante@gmail.com>
2024-12-08 22:36:27 +0100

    stack correction for button flow

--- lib/presentation/splash/intro/pages/get_started.dart
+++ lib/presentation/splash/intro/pages/get_started.dart
@@ -4,6 +4,7 @@ import 'package:ubify/common/widgets/button/basic_app_button.dart';
 import 'package:ubify/core/configs/theme/app_colors.dart';
 import 'package:ubify/core/configs/theme/assets/app_images.dart';
 import 'package:ubify/core/configs/theme/assets/app_vectors.dart';
+import 'package:ubify/presentation/splash/choose_mode/pages/choose_model.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -12,6 +13,7 @@ class GetStartedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //stack lets us to draw the filter container after the image one
       body: Stack(
         children: [
           Container(
@@ -23,47 +25,54 @@ class GetStartedPage extends StatelessWidget {
               image: DecorationImage(
                   fit: BoxFit.fill, image: AssetImage(AppImages.introBG)),
             ),
-            child: Column(
-              children: [
-                const SizedBox(
-                  width: 10.0, // Para espaço horizontal
-                  height: 10.0, // Para espaço vertical
-                ),
-                Align(
-                    alignment: Alignment.topCenter,
-                    child: SvgPicture.asset(AppVectors.spotifyLogo)),
-                const Spacer(),
-                const Text(
-                  'Enjoy Listening to Music',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 18),
-                ),
-                const SizedBox(
-                  height: 21,
-                ),
-                const Text(
-                  'Enjoy the best music from ubi!\n our special music here is "So fe Irmao"',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.grey,
-                      fontSize: 13),
-                  textAlign: TextAlign.center,
-                ),
-                //button shld be bellow the 2 text promts
-                const SizedBox(
-                  height: 20,
-                ),
-                BasicAppButton(
-                    onPressed: () {}, //Create the ChoseModePage
-                    title: 'Get Started'),
-              ],
-            ),
           ),
+          //this container fills all the area from above container
           Container(
               color: Colors.black.withOpacity(0.15) //operates like a filter
               ),
+          Column(
+            children: [
+              const SizedBox(
+                width: 10.0, // Para espaço horizontal
+                height: 10.0, // Para espaço vertical
+              ),
+              Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.spotifyLogo)),
+              const Spacer(),
+              const Text(
+                'Enjoy Listening to Music',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18),
+              ),
+              const SizedBox(
+                height: 21,
+              ),
+              const Text(
+                'Enjoy the best music from ubi!\n our special music here is "So fe Irmao"',
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.grey,
+                    fontSize: 13),
+                textAlign: TextAlign.center,
+              ),
+              //button shld be bellow the 2 text promts
+              const SizedBox(
+                height: 20,
+              ),
+              BasicAppButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                const ChooseModePage()));
+                  }, //Create the ChoseModePage
+                  title: 'Get Started'),
+            ],
+          ),
         ],
       ),
     );
