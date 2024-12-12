336ba0a8d65cecdc4ee5723b54177755ef042065
Leonardo Gonçalves <leonardo.elcaminante@gmail.com>
2024-12-12 20:47:26 +0100

    Adding signin page

---
+++ lib/presentation/auth/pages/signin.dart
@@ -0,0 +1,126 @@
+import 'dart:ffi';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:ubify/common/widgets/appbar/app_bar.dart';
+import 'package:ubify/common/widgets/button/basic_app_button.dart';
+import 'package:ubify/core/configs/theme/app_theme.dart';
+import 'package:ubify/core/configs/theme/assets/app_vectors.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: _signinText(context),
+      //logo in appbar
+      //we will pass one widget to BasicAppBar
+      appBar: BasicAppBar(
+        //removes cosnt from scaffold bcs tittle depends on external assets
+        //so we remove it to construct in real time compilation
+        title: SvgPicture.asset(
+          AppVectors.blackSpotifyLogo,
+          width: 70,
+          height: 70,
+        ),
+      ),
+      //dist from the appbar
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _registerText(),
+            _registerInputFieldsSpace(),
+            _fullNameField(context),
+            _InputFieldsSpace(),
+            _emailField(context),
+            _InputFieldsSpace(),
+            _passwordField(context),
+            _InputFieldsSpace(),
+            BasicAppButton(onPressed: () {}, title: "Create Acounte"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _registerText() {
+    return const Text(
+      "Register",
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 25,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  //BUILDCONTEXT context lets us get access to the current theme and widget tree
+  Widget _fullNameField(BuildContext context) {
+    //TO ACCESS THE CURRENT THEME
+    //as it is alredy defined I dont need to define anythin
+    //and theme is alredy choosed
+    return TextField(
+      decoration: const InputDecoration(hintText: "Full Name")
+          .applyDefaults(Theme.of(context).inputDecorationTheme),
+    );
+    //APLLY defaults it for
+    //use the propertyes from the context's theme on  context.inputDecorationTheme
+    //like color edges border for the dark and light theme
+    //if not defined we will define this input Decorations
+  }
+
+  Widget _emailField(BuildContext context) {
+    return TextField(
+      decoration: const InputDecoration(hintText: "Enter Email")
+          .applyDefaults(Theme.of(context).inputDecorationTheme),
+    );
+  }
+
+  Widget _passwordField(BuildContext context) {
+    return TextField(
+      decoration: const InputDecoration(hintText: "Enter Password")
+          .applyDefaults(Theme.of(context).inputDecorationTheme),
+    );
+  }
+
+  Widget _registerInputFieldsSpace() {
+    return const SizedBox(
+      height: 50,
+    );
+  }
+
+  Widget _InputFieldsSpace() {
+    return const SizedBox(
+      height: 20,
+    );
+  }
+
+  Widget _signinText(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 30,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Do you alredy have an account?",
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 14,
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "Sign in",
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
