fc94bf64354e35676e948417ad92af0b26729baa
Leonardo Gonçalves <leonardo.elcaminante@gmail.com>
2024-12-20 14:19:31 +0100

    domain conf

---
+++ lib/data/models/auth/signin_user_req.dart
@@ -0,0 +1,6 @@
+class SigninUserReq {
+  //prompters from page
+  final String email;
+  final String password;
+  SigninUserReq({required this.email, required this.password});
+}
--- lib/data/sources/auth/auth_firebase_service.dart
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:ubify/data/models/auth/create_user_req.dart';
+import 'package:ubify/data/models/auth/signin_user_req.dart';
 
 //how to create a service for authentication
 //SERVICE encapsulates the interaction with the API
@@ -9,14 +10,26 @@ abstract class AuthFirebaseService {
   //2 methods
   //we need to pass an instance of the create user obj to singup
   Future<Either> signup(CreateUserReq createUserReq);
-  Future<void> signin();
+  Future<Either> signin(SigninUserReq signinUserReq);
 }
 
 class AuthFireBaseServiceImpl extends AuthFirebaseService {
   @override
-  Future<void> signin() {
-    // TODO: implement signin
-    throw UnimplementedError();
+  Future<Either> signin(SigninUserReq signinUserReq) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: signinUserReq.email, password: signinUserReq.password);
+      return const Right("Sign in was successfull");
+    } on FirebaseAuthException catch (e) {
+      String message = "";
+      if (e.code == "weak-password") {
+        message = "The password provided is too weak";
+      } else if (e.code == "email-alredy-in-use") {
+        message = "An account alredy exists with that email";
+      }
+      //retorna a message
+      return Left(message);
+    }
   }
 
   //como ele recebe um user com os parametros ja posso meter
