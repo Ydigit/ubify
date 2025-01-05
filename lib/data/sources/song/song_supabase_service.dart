49902df762b1987f5e6c1ff1cde1acc906a3e179
Leonardo Gonçalves <leonardo.elcaminante@gmail.com>
2025-01-05 20:00:51 +0100

    get list is doen backend

--- lib/data/sources/song/song_supabase_service.dart
+++ lib/data/sources/song/song_supabase_service.dart
@@ -5,6 +5,8 @@ import 'package:ubify/service_locator.dart';
 
 abstract class SongSupabaseService {
   Future<Either<String, List<SongEntity>>> getNewsSongs();
+  //new method get playlist
+  Future<Either<String, List<SongEntity>>> getPlayList();
 }
 
 class SongSupabaseServiceImpl extends SongSupabaseService {
@@ -14,6 +16,23 @@ class SongSupabaseServiceImpl extends SongSupabaseService {
 
   @override
   Future<Either<String, List<SongEntity>>> getNewsSongs() async {
+    try {
+      // Busca músicas usando o SupabaseMusicService
+      final songsData = await musicService.fetchSongs();
+
+      // Converte os dados em SongEntity
+      final songs =
+          songsData.map((song) => SongModel.fromJson(song).toEntity()).toList();
+
+      return Right(songs);
+    } catch (e) {
+      return Left('Erro ao buscar músicas: $e');
+    }
+  }
+
+//get list method _> esta definido no service locator
+  @override
+  Future<Either<String, List<SongEntity>>> getPlayList() async {
     try {
       // Busca músicas usando o SupabaseMusicService
       final songsData = await musicService.fetchSongs();
