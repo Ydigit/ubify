c9fee17288a6fa32f8bd197632005c694f932f25
Leonardo Gonçalves <leonardo.elcaminante@gmail.com>
2025-01-05 01:58:37 +0100

    top tab is done

--- lib/core/configs/theme/assets/app_images.dart
+++ lib/core/configs/theme/assets/app_images.dart
@@ -4,4 +4,6 @@ class AppImages {
   static const String introBG = '${basePath}intro_bg.png';
   static const String chooseModeBG = '${basePath}choose_color_bg.png';
   static const String authImage = '${basePath}auth_bg.png';
+  static const String homeArtist = '${basePath}Home_artist.png';
+  static const String homeArtist2 = '${basePath}Home_artist.png';
 }
--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:ubify/common/widgets/appbar/app_bar.dart';
+import 'package:ubify/core/configs/theme/assets/app_images.dart';
 import 'package:ubify/core/configs/theme/assets/app_vectors.dart';
 
 //root class after login
@@ -10,18 +11,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appbar
-      appBar: BasicAppBar(
-        hideBackArrow: true,
-        //removes cosnt from scaffold bcs tittle depends on external assets
-        //so we remove it to construct in real time compilation
-        //remove or jide the back button
-        title: SvgPicture.asset(
-          AppVectors.blackSpotifyLogo,
-          width: 70,
-          height: 70,
+        //appbar
+        appBar: BasicAppBar(
+          hideBackArrow: true,
+          //removes cosnt from scaffold bcs tittle depends on external assets
+          //so we remove it to construct in real time compilation
+          //remove or jide the back button
+          title: SvgPicture.asset(
+            AppVectors.blackSpotifyLogo,
+            width: 70,
+            height: 70,
+          ),
         ),
-      ),
+        body: SingleChildScrollView(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _homeTopCard(),
+          ],
+        )));
+  }
+
+  Widget _homeTopCard() {
+    return Center(
+      child: SizedBox(
+          height: 140,
+          child: Stack(
+            children: [
+              Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SvgPicture.asset(AppVectors.homeTopCard)),
+              Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 60),
+                    child: Image.asset(AppImages.homeArtist),
+                  ))
+            ],
+          )),
     );
   }
 }
