9e523528d310e9bc19ae4ed73e781326b8f41a50
Leonardo Gonçalves <leonardo.elcaminante@gmail.com>
2025-01-05 19:22:21 +0100

    button is done, now need to implement playlist by release date

--- lib/presentation/home/widgets/news_songs.dart
+++ lib/presentation/home/widgets/news_songs.dart
@@ -2,7 +2,10 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ubify/common/helpers/is_dark_mode.dart';
+import 'package:ubify/common/widgets/appbar/app_bar.dart';
 import 'package:ubify/core/configs/constants/app_urls.dart';
+import 'package:ubify/core/configs/theme/app_colors.dart';
 import 'package:ubify/domain/entities/song/song.dart';
 import 'package:ubify/presentation/home/bloc/news_songs_cubit.dart';
 import 'package:ubify/presentation/home/bloc/news_songs_state.dart';
@@ -21,7 +24,7 @@ class NewsSongs extends StatelessWidget {
               if (state is NewsSongsLoading) {
                 return Container(
                     alignment: Alignment.center,
-                    child: CircularProgressIndicator());
+                    child: const CircularProgressIndicator());
               }
               if (state is NewsSongsLoaded) {
                 return _songs(state.songs);
@@ -53,28 +56,49 @@ class NewsSongs extends StatelessWidget {
                     borderRadius: BorderRadius.circular(
                         8), // Opcional para bordas arredondadas
                   ),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      transform: Matrix4.translationValues(10, 10, 0),
+                      child: Icon(
+                        Icons.play_arrow_rounded,
+                        color: context.isDarkMode
+                            ? const Color(0xff959595)
+                            : const Color(0xff555555),
+                      ),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: context.isDarkMode
+                              ? AppColors.darkGrey
+                              : Colors.white),
+                    ),
+                  ),
                 ),
               ),
-              SizedBox(height: 8),
+              const SizedBox(height: 8),
               Padding(
                 padding: const EdgeInsets.only(left: 3, right: 3),
                 child: Text(
                   songs[index].title,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w500),
                   overflow: TextOverflow.ellipsis,
                 ),
               ),
-              SizedBox(height: 8),
+              const SizedBox(height: 8),
               Text(
                 songs[index].artist,
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+                style:
+                    const TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
                 overflow: TextOverflow.ellipsis,
               ),
             ],
           ),
         );
       },
-      separatorBuilder: (context, index) => SizedBox(width: 14),
+      separatorBuilder: (context, index) => const SizedBox(width: 14),
       itemCount: songs.length,
     );
   }
